1ad1d4417953151ee0fc30c4cc3963fde6fdad33
Anton <61601837+BodunOk@users.noreply.github.com>
2020-05-15 16:21:27 +0300

    Update lab4.cpp

--- Linux/lab4.cpp
+++ Linux/lab4.cpp
@@ -3,26 +3,26 @@
 #include <stack>
 #include <unistd.h>
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex; //создание мьютекса для синхронизации потоков
 int thread;
 
  void* printThread(void*) {
     thread++;
     std::string threadNumber = std::to_string(thread) + " thread";
     while (true) {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex); //занимаем мьютекс или ждём его освобождения а потом занимаем
         for(char i : threadNumber) {
             std::cout << i;
         }
         std::cout << std::endl;
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex); //освобождаем мьютекс
         usleep(200000);
     }
 }
 
 int main() {
-    pthread_t threadId = 0;
-    std::stack<pthread_t> threadsStack;
+    pthread_t threadId = 0; //указатель по которому будет записан идентификатор потока
+    std::stack<pthread_t> threadsStack; 
     char branch;
     std::cout << "+ - create thread\n- - terminate last thread\nq - terminate all threads\n";
     pthread_mutex_init(&mutex,nullptr);
@@ -31,14 +31,14 @@ int main() {
         std::cin >> branch;
         switch(branch) {
             case '+' :
-                pthread_create(&threadId,nullptr,printThread,nullptr);
+                pthread_create(&threadId,nullptr,printThread,nullptr); //создаём новый поток
                 threadsStack.push(threadId);
                 threadId++;
                 break;
 
             case '-' :
                 if(!threadsStack.empty()) {
-                    pthread_cancel(threadsStack.top());
+                    pthread_cancel(threadsStack.top()); //завершаем последний созданный поток
                     threadsStack.pop();
                 }
                 if(threadsStack.empty()) {
@@ -51,7 +51,7 @@ int main() {
                     pthread_cancel(threadsStack.top());
                     threadsStack.pop();
                 }
-                pthread_mutex_destroy(&mutex);
+                pthread_mutex_destroy(&mutex); //уничтожаем мъютекс
                 return 0;
 
             default :
